bb730092843b8f8629453bfbd1897ed038020f83
DrumClock <64655979+DrumClock@users.noreply.github.com>
2026-09-04 08:46:26 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,11 @@
 // Jednoduchý offline cache pro appku "Kdy můžu domů?"
-const CACHE = 'kdy-domu-v7';
+const CACHE = 'kdy-domu-v8';
 const ASSETS = [
   'index.html',
   'sdilet.html',
   'napoveda.html',
   'zaloha.html',
+  'stavy.html',
   'pdf-import.html',
   'qr.png',
   'manifest.json',
